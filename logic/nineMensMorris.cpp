9fbc87879af7dc0a40bd7fb206edf189bfb8d773
AmirMahdi <carbon128x@gmail.com>
2026-07-16 21:24:33 +0330

    implementation of private helper functions

--- logic/nineMensMorris.cpp
+++ logic/nineMensMorris.cpp
@@ -39,3 +39,103 @@ const vector<vector<int>> POS_NEIGHBORS = {      // neighbors of each position
 
 // If 100 moves passed with no piece removed, the game is draw.
 static const int DRAW_LIMIT = 100;
+
+
+NineMensMorris::NineMensMorris() {
+    for (int i = 0; i < 24; i++)
+        board_[i] = 0;
+    current_ = 1;
+    toPlace_[0] = 0;
+    toPlace_[1] = 9;
+    toPlace_[2] = 9;
+    awaitingRemoval_ = false;
+    movesSinceRemoval_ = 0;
+}
+
+
+int NineMensMorris::otherPlayer(int player) const {
+    if (player == 1)
+        return 2;
+    else
+        return 1;
+}
+
+int NineMensMorris::countPieces(int player) const {
+    int count = 0;
+    for (int i = 0; i < 24; i++)
+        if (board_[i] == player)
+            count++;
+    return count;
+}
+
+bool NineMensMorris::inPlacingPhase(int player) const {
+    if (toPlace_[player] > 0)
+        return true;
+    else
+        return false;
+}
+
+bool NineMensMorris::canFly(int player) const {
+    if (!inPlacingPhase(player) && (countPieces(player) == 3)) // placing should be already done
+        return true;
+    else
+        return false;
+}
+
+bool NineMensMorris::areNeighbours(int a, int b) const {
+    const vector<int>& nb = POS_NEIGHBORS[a];
+    int vectorSize = (int)nb.size();
+    for (int i = 0; i < vectorSize; i++)
+        if (nb[i] == b)
+            return true;
+
+    return false;
+}
+
+bool NineMensMorris::formsMill(int pos, int player) const {
+    for (int i = 0; i < 16; i++)
+        if (pos == MILLS[i][0] || pos == MILLS[i][1] || pos == MILLS[i][2])
+            if (board_[MILLS[i][0]] == player && board_[MILLS[i][1]] == player && board_[MILLS[i][2]] == player)
+                return true;
+
+    return false;
+}
+
+bool NineMensMorris::isRemovable(int pos, int opponent) const {
+    if (board_[pos] != opponent)
+        return false;
+    if (!formsMill(pos, opponent))
+        return true;
+
+    for (int i = 0; i < 24; i++)      // if it was in a mill, we should check all of enemy pieces if they're in mill
+        if (board_[i] == opponent && !formsMill(i, opponent))      // if one of the others wasn't in mill, so pos isn't removable
+            return false;
+    return true;
+}
+
+vector<Move> NineMensMorris::movement(int player) const {      // legal moves but without checking gameOver(for after placing)
+    vector<Move> moves;
+
+    bool fly = canFly(player);
+
+    for (int from = 0; from < 24; from++)
+    {
+        if (board_[from] != player)
+            continue;
+
+        if (fly)
+            for (int to = 0; to < 24; to++)
+                if (board_[to] == 0)
+                    moves.push_back(Move{ from, to });
+        else 
+        {
+            const vector<int>& neighbor = POS_NEIGHBORS[from];
+            
+            for (int i = 0; i < (int)neighbor.size(); i++)
+                if (board_[neighbor[i]] == 0)
+                    moves.push_back(Move{ from, neighbor[i] });
+        }
+    }
+
+    return moves;
+}
