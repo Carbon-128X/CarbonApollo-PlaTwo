303985eb225c27e83ca44ff7aefbf3d53e9ca6d8
Amirali <gamirali158@gmail.com>
2026-07-19 17:42:10 +0330

    Some works on UI for Boxes , login , Morris, Fanorona, and the game window.

--- Platwo/main.cpp
+++ Platwo/main.cpp
@@ -3,19 +3,10 @@
 #include "usermanager.h"
 #include <QFile>
 #include <QDebug>
-#include <QDir>
 int main(int argc, char *argv[])
 {
-
-    qDebug() << "ROOT:";
-    qDebug() << QDir(":/").entryList(QDir::AllEntries);
-
-    qDebug() << "IMAGES:";
-    qDebug() << QDir(":/images").entryList(QDir::AllEntries);
-
     QApplication a(argc, argv);
     UserManager::load();
-
     Widget w;
     w.show();
 
